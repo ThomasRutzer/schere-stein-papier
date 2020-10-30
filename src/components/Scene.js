3efa352448251ae0848a6ec8cac4c5698a0f3a8d
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-10-31 00:47:17 +0100

    Update Scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Canvas } from "react-three-fiber"
-import { Text } from "@react-three/drei"
 
 import Oppenent from "./Oppenent"
 import CameraByFrameRotation from "./CameraByFrameRotation"
@@ -10,13 +9,6 @@ function Scene() {
     <Canvas >
       <CameraByFrameRotation />
       <ambientLight />
-      {/* <Text
-        color="#FE9F1B" // default
-        anchorX="center" // default
-        anchorY="middle" // default
-      >
-        START
-</Text> */}
       <pointLight position={[10, 10, 10]} />
       <Oppenent />
     </Canvas>
