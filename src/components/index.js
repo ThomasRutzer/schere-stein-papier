cab1cc7d11ba4368ac3890392b9656d1725744c1
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-10-30 18:30:13 +0100

    Style updates

--- src/components/index.js
+++ src/components/index.js
@@ -1,29 +1,13 @@
-import { useEffect, useRef } from "react"
-import anime from "animejs"
-
 import useStore from "../../store"
-import { CONTROL_REVEAL_TRANSITION_DURATION } from "./../../settings"
 import "./index.css"
 import StartButton from "./StartButton"
 import YouSelect from "../YouSelect"
 
 const YourControls = () => {
   const state = useStore()
-  const wrapper = useRef()
-
-  useEffect(() => {
-    if (state.sceneLoaded) {
-      anime({
-        targets: wrapper.current,
-        translateY: ["100%", 0],
-        opacity: 1,
-        duration: CONTROL_REVEAL_TRANSITION_DURATION
-      })
-    }
-  }, [state.sceneLoaded, wrapper])
 
   return (
-    <div ref={wrapper} className="your-controls">
+    <div className="your-controls">
       { (!state.othersIsChoosing && !state.youAreChoosing) &&
         <StartButton />
       }
