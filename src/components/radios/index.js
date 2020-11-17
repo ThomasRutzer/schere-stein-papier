6cd2308dc6554a2f2b1601480173168cffba0608
Thomas Rutzer <thomas.rutzer@artcom.de>
2020-11-17 22:07:47 +0100

    Hide radios after timeout runs out

--- src/components/radios/index.js
+++ src/components/radios/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState, useRef, useCallback } from "react"
 import anime from "animejs"
 
 import {
@@ -32,7 +32,12 @@ const YouSelect = () => {
   useEffect(() => {
     const exactTimeout = TIME_TO_CHOOSE - (Math.abs(Date.now() - now))
     timeToChooseTimeout.current = setTimeout(() => {
-      saveYours(YOUR_SELECTION_NOT_SELECTED)
+      const targets = wrapper.current.childNodes
+      const onRadiosHidden = () => {
+        saveYours(YOUR_SELECTION_NOT_SELECTED)
+      }
+
+      hideRadios(targets, onRadiosHidden)
     }, exactTimeout)
 
     return () => clearTimeout(timeToChooseTimeout.current)
@@ -41,18 +46,24 @@ const YouSelect = () => {
   const handleChange = e => {
     const selection = e.target.value
     const targets = Array.from(wrapper.current.childNodes).filter(child => !child.classList.contains(`radio--${selection}`))
+    const onRadiosHidden = () => {
+      clearTimeout(timeToChooseTimeout.current)
+      saveYours(selection)
+    }
+
+    hideRadios(targets, onRadiosHidden)
+  }
+
+  const hideRadios = useCallback((targets, onComplete) => {
     anime({
       targets,
       translateY: [0, 250],
       duration: 700,
       easing: "easeOutQuad",
       delay: anime.stagger(50),
-      complete: () => {
-        clearTimeout(timeToChooseTimeout.current)
-        saveYours(selection)
-      },
+      complete: onComplete
     })
-  }
+  }, [])
 
   return (
     <div ref={wrapper} className="radios">
